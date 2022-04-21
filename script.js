d7cce015b03e9b8d633460226e5620e3810f2c04
Corey Schadeck <corey@schadeck.com>
2022-04-21 18:14:15 -0400

    updates and redesigne

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
 const h2 = document.createElement("h2");
 h2.textContent = "This content added by JavaScript";
     document.querySelector("body").appendChild(h2);
+
+
+// grab button elements
+const buddyButton = document.querySelector('#buddyButton');
+const buttercupButton  = document.querySelector('#buttercupButton');
+const buddyPix = document.querySelector('#buddyPix');
+const buttercupPix = document.querySelector('#buttercupPix');
+
+// assign events
+buddyButton.addEventListener('click', function(e) {
+    buddyPix.classList.toggle('hide');
+});
+
+buttercupButton.addEventListener('click', function(e) {
+    buttercupPix.classList.toggle('hide');
+});
