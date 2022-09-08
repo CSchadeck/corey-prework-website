be78896948cc9af08eeec90d64d2232ad9cf0923
Corey Schadeck <corey@schadeck.com>
2022-09-08 14:49:23 -0400

    buttercups pictures and footer

--- script.js
+++ script.js
@@ -1,8 +1,3 @@
-const h2 = document.createElement("h2");
-h2.textContent = "This content added by JavaScript";
-    document.querySelector("body").appendChild(h2);
-
-
 // grab button elements
 const buddyButton = document.querySelector('#buddyButton');
 const buttercupButton  = document.querySelector('#buttercupButton');
